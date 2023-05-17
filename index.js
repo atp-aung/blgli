0d7f92bcb18eeb85a1000032b0958fd499f4115b
atp-aung <76736708+atp-aung@users.noreply.github.com>
2023-05-17 21:38:40 +0630

    a

--- index.js
+++ index.js
@@ -1,28 +1,7 @@
-const express = require("express");
-const app = express();
-const cors = require("cors");
+const app = require("./app"); // the actual Express application
+const config = require("./utils/config");
+const logger = require("./utils/logger");
 
-//const Blog =
-
-mongoose.connect(mongoUrl);
-
-app.use(cors());
-app.use(express.json());
-
-app.get("/api/blogs", (request, response) => {
-  Blog.find({}).then((blogs) => {
-    response.json(blogs);
-  });
-});
-
-app.post("/api/blogs", (request, response) => {
-  const blog = new Blog(request.body);
-
-  blog.save().then((result) => {
-    response.status(201).json(result);
-  });
-});
-
-app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
+app.listen(config.PORT, () => {
+  logger.info(`Server running on port ${config.PORT}`);
 });
