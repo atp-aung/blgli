ed207cf34ea02331e0d1c088387106abc3b23c17
atp-aung <76736708+atp-aung@users.noreply.github.com>
2023-05-21 22:05:15 +0630

    a

--- utils/list_helper.js
+++ utils/list_helper.js
@@ -6,11 +6,21 @@ const dd = (blogs) => {
   return 2;
 };
 
+const favBlgs = (blogs) => {
+  const b = Math.max(...blogs.map((o) => o.likes));
+  const idx = blogs.findIndex((o) => o.likes === b);
+  const obj = {
+    title: blogs[idx].title,
+    author: blogs[idx].author,
+    likes: blogs[idx].likes,
+  };
+  return obj;
+};
+
 const ttlikes = (ar) => {
   const reducer = (sum, item) => {
     return sum + item.likes;
   };
-
   return ar.reduce(reducer, 0);
 };
 
@@ -18,4 +28,5 @@ module.exports = {
   dummy,
   ttlikes,
   dd,
+  favBlgs,
 };
