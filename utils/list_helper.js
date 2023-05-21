ecdf76f5cc17883a365344ba691a535c58c9d78c
atp-aung <76736708+atp-aung@users.noreply.github.com>
2023-05-21 15:51:24 +0630

    a

--- utils/list_helper.js
+++ utils/list_helper.js
@@ -2,6 +2,20 @@ const dummy = (blogs) => {
   return 1;
 };
 
+const dd = (blogs) => {
+  return 2;
+};
+
+const ttlikes = (ar) => {
+  const reducer = (sum, item) => {
+    return sum + item.likes;
+  };
+
+  return ar.reduce(reducer, 0);
+};
+
 module.exports = {
   dummy,
+  ttlikes,
+  dd,
 };
