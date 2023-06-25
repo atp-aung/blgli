6776842ed9547f04b7e667b1ec886c1f0c3e0797
atp-aung <76736708+atp-aung@users.noreply.github.com>
2023-06-25 21:19:36 +0630

    x

--- controllers/blogs.js
+++ controllers/blogs.js
@@ -6,7 +6,7 @@ blogsRouter.get("/", async (request, response) => {
   response.json(blogs);
 });
 
-blogsRouter.post("/", async (request, response, next) => {
+blogsRouter.post("/", async (request, response) => {
   const body = request.body;
   const blog = new Blog({
     title: body.title,
@@ -14,12 +14,8 @@ blogsRouter.post("/", async (request, response, next) => {
     url: body.url,
     likes: body.likes,
   });
-  try {
-    const savedBlog = await blog.save();
-    response.status(201).json(savedBlog);
-  } catch (exception) {
-    next(exception);
-  }
+  const savedBlog = await blog.save();
+  response.status(201).json(savedBlog);
 });
 
 module.exports = blogsRouter;
